76536df7bd22eb5aeeac3f718039509b2876f524
Kgaogelo072 <140261103+Kgaogelo072@users.noreply.github.com>
2024-04-05 10:08:07 +0200

    third commit

---
+++ 1. LongestSentanceTests.cs
@@ -0,0 +1,17 @@
+﻿using TS.TechnicalTest;
+
+namespace TS.UnitTests;
+
+[TestClass]
+public class LongestSentanceTests
+{
+    [TestMethod]
+    public void Q1_BasicNominalTest()
+    {
+
+        //var input = "Forget CSs..Save time .xx";
+        var input = $"We test coders. Give us a try";
+        var outcome = LongestSentanceAnswer.Solution(input);
+        Assert.AreEqual(4, outcome);
+    }
+}

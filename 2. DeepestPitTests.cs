1e96c94f8caa18239507eadf2a5c18068cf8973f
Kgaogelo072 <140261103+Kgaogelo072@users.noreply.github.com>
2024-04-05 11:35:50 +0200

    eleventh commit

--- 2. DeepestPitTests.cs
+++ 2. DeepestPitTests.cs
@@ -8,9 +8,11 @@ public class DeepestPitTests
     [TestMethod]
     public void Q2_BasicNominalTest()
     {
-        var points = new[] {0, 1, 3, -2, 0, 1, -3, 2, 3};
+        //var points = new[] {0, 1, 3, -2, 0, 1, -3, 2, 3};
+        var points = new[] {1, 2, 3, 1, 2, 3, 1, 2, 3};
         var result = DeepestPitAnswer.Solution(points);
 
-        Assert.AreEqual(4, result);
+        //Assert.AreEqual(4, result);
+        Assert.AreEqual(2, result);
     }
 }
